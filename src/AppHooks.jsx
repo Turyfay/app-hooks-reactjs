02b79aed50cee56f63477b40f86c47bb7663ff64
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-21 08:01:20 -0600

    add: uso del hook useRef

--- src/AppHooks.jsx
+++ src/AppHooks.jsx
@@ -1,11 +1,12 @@
 import { MultiCustomHook } from "./examples/MultiCustomHook"
 import { FormCustomHook } from "./useEffect"
+import { FocusScreen } from "./useRef/FocusScreen"
 /* import { CounterAppCustom } from "./useState"
  */
 export const AppHooks = () => {
   return (
     <>
-    <MultiCustomHook/>
+    <FocusScreen/>
     </>
   )
 }
