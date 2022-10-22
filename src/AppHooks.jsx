d516ff899bacd182f9ee0f2124d1454eb921ccb2
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-21 19:32:33 -0600

    add: creacion de la tarea

--- src/AppHooks.jsx
+++ src/AppHooks.jsx
@@ -1,10 +1,11 @@
 
+import { Padre } from "./07-tarea-memo/Padre"
 import { CallbackHook } from "./memos/CallbackHook"
 import { MemoHook } from "./memos/MemoHook"
 export const AppHooks = () => {
   return (
     <>
-    <CallbackHook/>
+    <Padre />
     </>
   )
 }
--- src/memos/CallbackHook.jsx
+++ src/memos/CallbackHook.jsx
@@ -5,8 +5,8 @@ export const CallbackHook = () => {
     const [counter, setCounter] = useState(10);
 
     const incrementar = useCallback(
-      () => {
-        setCounter((value)=> value + 1);
+      (value) => {
+        setCounter((c)=> c + value);
       },
       [],
     )
