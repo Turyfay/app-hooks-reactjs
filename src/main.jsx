a15e72a67f5abaeb9566672fcc07de63f1462063
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-25 00:13:31 -0600

    add: intro del uso del use reducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { AppHooks } from './AppHooks'
 import './index.css'
 
+import './useReducer/intro-reducer';
+
+/* 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <AppHooks />
   </React.StrictMode>
 )
+ */
