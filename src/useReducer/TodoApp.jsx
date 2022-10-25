485a70b516d636f6669817b67e787758abeef38c
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-25 17:06:19 -0600

    add: guardar los todo en el localstorage

--- src/useReducer/TodoApp.jsx
+++ src/useReducer/TodoApp.jsx
@@ -1,27 +1,24 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { TodoAdd, TodoList } from "../components"
 import { todoReducer } from "./todoReducer"
 
 
-const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del Alma',
-        done: false
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Hacer algo mas',
-        done: false
-    },
+const initialState = [];
 
-]
+const init = ()=>{
+    return JSON.parse(localStorage.getItem('todos') || []);
+}
 
 
 export const TodoApp = () => {
 
 
-    const [state, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState,init);
+
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos));
+    }, [todos])
+    
 
     const handleNewTodo = (todo)=>{
         dispatch({type: 'add', payload:todo});
@@ -34,7 +31,7 @@ export const TodoApp = () => {
             <div className="row">
                 <div className="col-7">
                   {/* Todo List */}
-                  <TodoList todos={state} />
+                  <TodoList todos={todos} />
                 </div>
 
                 <div className="col-5">
