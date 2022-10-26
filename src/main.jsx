9c9d036894d97402755e8fe14e7028b563992987
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-26 09:48:04 -0600

    add: creacion de los componentes de navegacion e instalacion de react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AppHooks } from './AppHooks'
+import { MainApp } from './useContext'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 
-/* import './useReducer/intro-reducer';
- */
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <AppHooks />
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>  
   </React.StrictMode>
 )
